c1700bfd4daad6e11148bffe7512662db2b85521
devmetal <devmetal91@gmail.com>
2014-05-09 04:02:04 +0200

    Package updates

---
+++ example/example.dart
@@ -0,0 +1,48 @@
+import 'package:rest_let/restlet.dart';
+import 'package:route/server.dart';
+import 'dart:io';
+
+void main() {
+  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 4444)
+  .then((HttpServer server){
+    print("Address: http://${server.address.address}:${server.port}");
+    
+    Router router = new Router(server);
+    RestServer rest = new RestServer(router);
+    
+    rest.addResource(new Resource("GET", "/api/hello"))
+      .listen((ResourceEvent e){
+        var request = e.request;
+        print("Request on");
+        print(request.method);
+        print(request.uri.path);
+        
+        request.response
+          ..statusCode = 200
+          ..headers
+            .add('Content-Type', 'text/plain')
+          ..write("Hello to")
+          ..close();
+      });
+    
+    rest.addResource(new Resource("GET", "/api/hello/:name"))
+      .listen((ResourceEvent e){
+        var request = e.request;
+        print("Request on");
+        print(request.method);
+        print(request.uri.path);
+        
+        var route = e.route;
+        var name = route.params['name'].getValue();
+        
+        request.response
+          ..statusCode = 200
+          ..headers
+            .add('Content-Type', 'text/plain')
+          ..write("Hello to ${name}")
+          ..close();
+      });
+    
+    rest.start();
+  });
+}
---
+++ lib/router/route_resolver.dart
@@ -0,0 +1,15 @@
+part of restlet;
+
+class RouteResolver {
+  RouteResolver();
+  
+  void resolve(Route r, String path) {
+    List<String> parsed  = r.pattern.parse(path);
+    List<Param> compiled = r.compiled;
+    
+    parsed.forEach((e){
+      var i = parsed.indexOf(e);
+      compiled[i].setValue(e);
+    });
+  }
+}
